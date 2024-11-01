974efab42423c7922ed615633562c0359dd7cf23
BeyzCoder <stevenbaes11@yahoo.com>
2024-10-31 19:44:56 -0600

    Initial commit

---
+++ api/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+
+# Create a asynchronous server gateway interface
+app = FastAPI()
+
+@app.get("/")
+async def root() -> JSONResponse:
+    resp: dict = {"state" : "Server up running!"}
+    return JSONResponse(content=resp, status_code=status.HTTP_200_OK, media_type="application/json")
