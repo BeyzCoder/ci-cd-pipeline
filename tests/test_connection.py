e9ca564b0b5a1f713c7e216a11b712a390a70b89
BeyzCoder <stevenbaes11@yahoo.com>
2024-11-02 10:54:46 -0600

    Added workflow and sample unit test.

---
+++ tests/test_connection.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
+def test_root() -> None:
+    resp = client.get("/")
+    assert resp.status_code == 200
+    assert resp.json() == {"status" : "Server up running!"}
